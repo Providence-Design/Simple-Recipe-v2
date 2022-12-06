41b45e771bb9a8a74ac743abfdf1b713961071ad
Providence-Design <baabaprovidence@gmail.com>
2022-12-06 20:13:52 +0000

    refactored RecipeRepository class and cretaed queries classes

--- src/main/java/SimpleRecommendationSystem/recommendation/repository/IngredientRepository.java
+++ src/main/java/SimpleRecommendationSystem/recommendation/repository/IngredientRepository.java
@@ -1,28 +1,21 @@
 package SimpleRecommendationSystem.recommendation.repository;
 
 import SimpleRecommendationSystem.recommendation.model.Ingredient;
+import SimpleRecommendationSystem.recommendation.query.IngredientQuery;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public class IngredientRepository {
+
     private final JdbcTemplate jdbcTemplate;
-    private final static String LIST_OF_INGREDIENT_QUERY =
-            " SELECT ingredient.id, ingredient.name" + " " +
-                    "FROM recipe_ingredient" + " " +
-                    "INNER JOIN ingredient ON ingredient.id = recipe_ingredient.ingredient_id" + " " +
-                    "WHERE recipe_ingredient.recipe_id = ?";
 
     public IngredientRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
-
     }
+
     public List<Ingredient> getByRecipeId(int recipeId) {
-        return jdbcTemplate.query(LIST_OF_INGREDIENT_QUERY, new IngredientMapper(), recipeId);
-
+        return jdbcTemplate.query(IngredientQuery.LIST_OF_INGREDIENT_QUERY, new IngredientMapper(), recipeId);
     }
-
-
 }
